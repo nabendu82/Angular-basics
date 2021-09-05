e88cac20134e30dea82a12355c7a3a39985bbf5c
Nabendu <nabendu.biswas@gmail.com>
2021-09-05 11:44:05 +0530

    Angular Basics 2 complete

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  selector: '.app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverStatus = 'No server was created';
+  serverName = '';
+  serverCreated = false;
+  servers = ['SR1','SR2']
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverStatus = `Server ${this.serverName} created`;
+  }
+
+  onUpdate(event: any){
+    this.serverName = event.target.value;
   }
 
 }
